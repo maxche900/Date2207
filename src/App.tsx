b9c8ca50f8107aa5bfa0caf73a156fc76a577b05
maxche900 <fremax@gmail.com>
2024-06-02 22:32:23 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>maxche900 bot test</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Счетчик {count}
         </button>
       </div>
         {/* Here we add our button with alert callback */}
